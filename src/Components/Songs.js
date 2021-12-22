0e873c7974dea49c2588ba8a9b71940451ae01da
khalilov07 <khalilovakram07@gmail.com>
2021-12-22 17:20:02 +0600

    first commit

---
+++ src/Components/Songs.js
@@ -0,0 +1,48 @@
+const Songs = [
+  {
+    id: 1,
+    favourite: false,
+    songName: "Bella Ciao",
+    artist: "El Profesor",
+    song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2Fbellaciao.mp3?alt=media&token=e1dd8233-3521-4574-8957-d51af9a5db5c",
+    imgSrc:
+      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fmoneyheist.jpg?alt=media&token=32baf3d7-0ab5-47d5-ba48-554792cea117",
+  },
+  {
+    id: 4,
+    favourite: false,
+    songName: "I Am A Peaky Blinder",
+    artist: "Otnicka",
+    song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2Fpeaky-blinder.mp3?alt=media&token=2ddb5cfb-f4e0-4604-b77c-a7f63894d3ec",
+    imgSrc:
+      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fpeakyblinder.jpg?alt=media&token=b83fbf86-37ef-47c2-948e-3447a22a2d07",
+  },
+  {
+    id: 6,
+    songName: "Your Woman",
+    artist: "White Town",
+    song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2FWhite-Town-Your-Women.mp3?alt=media&token=91817445-98ba-4a99-a4b4-6218f2742eb3",
+    imgSrc:
+      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fwhitetown.jpg?alt=media&token=25e1ec0c-24ec-4a11-a7a7-e55ec1fc50cf",
+  },
+  {
+    id: 8,
+    favourite: false,
+    songName: "Mask Off",
+    artist: "Future",
+    song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2FFuture-Mask-Off.mp3?alt=media&token=92d8b60f-2bca-406f-914a-40bfb9a9324a",
+    imgSrc:
+      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fmask.jpg?alt=media&token=22f0d7be-74b8-4c0d-849d-b777370e9559",
+  },
+  {
+    id: 9,
+    favourite: false,
+    songName: "Hey Mama",
+    artist: "David Guetta",
+    song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2FHeyMamaRingtone.mp3?alt=media&token=850148f5-8a15-4f87-995d-711138d5d7a0",
+    imgSrc:
+      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fheymama.jpg?alt=media&token=2d8e8632-8051-4105-9760-3c715d797802",
+  },
+];
+
+export { Songs };
